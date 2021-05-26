e49f8b22f3d1576d18ddd3994988839f2115b2ac
abhay gupta <abhaygupta908@gmail.com>
2021-05-26 12:50:00 +0530

    pushing

---
+++ Introduction/src/gitHubpro/calculator.java
@@ -0,0 +1,12 @@
+package gitHubpro;
+
+public class calculator {
+       public static int add(int a,int b) {
+    	   int c=a+b;
+    	   return c;
+       }
+       public static int multiply(int a,int b) {
+    	   int c=a*b;
+    	   return c;
+       }
+}
---
+++ Introduction/src/gitHubpro/main.java
@@ -0,0 +1,14 @@
+package gitHubpro;
+
+import java.util.Scanner;
+
+public class main extends calculator{
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		 main s=new main();
+		 System.out.println(s.add(2,5));
+		 
+	}
+
+}
